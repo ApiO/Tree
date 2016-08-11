c92129e0144755aa7dd67658544d1a61af642fec
ApiO <ac_flood@hotmail.fr>
2016-08-11 10:15:15 +0200

    Abb Nunit test project

--- Tree/Tree.NUnit/Properties/AssemblyInfo.cs
+++ Tree/Tree.NUnit/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tree.Tests")]
+[assembly: AssemblyTitle("Tree.NUnit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tree.Tests")]
+[assembly: AssemblyProduct("Tree.NUnit")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("01423ac4-84d3-40ba-b450-a4a1128e04cf")]
+[assembly: Guid("40e5f71f-f540-4d8f-bff0-7139d1461988")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Tree/Tree.NUnit/TreeTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tree.NUnit
+{
+    [TestFixture]
+    public class TreeTest
+    {
+        #region Data setup
+
+        private class Dummy
+        {
+            public string Foo { get; set; }
+        }
+
+        private Tree<Dummy> _tree = new Tree<Dummy>();
+
+        private long _rootId;
+        private Dummy _rootData;
+
+        private long _child1Id;
+        private Dummy _child1Data;
+
+        private long _child2Id;
+        private Dummy _child2Data;
+
+        #endregion
+
+        [SetUp]
+        protected void SetUp()
+        {
+            _rootId = 42;
+            _rootData = new Dummy { Foo = "root" };
+
+            _child1Id = 1;
+            _child1Data = new Dummy { Foo = "child 1" };
+
+            _child2Id = 2;
+            _child2Data = new Dummy { Foo = "child 2" };
+
+            _tree = new Tree<Dummy>();
+
+            _tree.AddChild(null, _rootId, _rootData);
+            _tree.AddChild(_rootId, _child1Id, _child1Data);
+            _tree.AddChild(_rootId, _child2Id, _child2Data);
+
+        }
+
+
+        [Test]
+        public void RootsDefined()
+        {
+            Assert.IsNotNull(_tree.Roots);
+            Assert.IsTrue(_tree.Roots.Count == 1);
+            Assert.IsTrue(_tree.Roots[0] == _rootId);
+        }
+
+        [Test]
+        public void LeavesDefined()
+        {
+            Assert.IsNotNull(_tree.Leaves);
+            Assert.IsTrue(_tree.Leaves.Count == 2);
+            Assert.IsTrue(_tree.Leaves[0] == _child1Id);
+            Assert.IsTrue(_tree.Leaves[1] == _child2Id);
+        }
+
+        [Test]
+        public void CanNavigateById()
+        {
+            Assert.IsTrue(_tree[_rootId].Data == _rootData);
+            Assert.IsTrue(_tree[_child1Id].Data == _child1Data);
+            Assert.IsTrue(_tree[_child2Id].Data == _child2Data);
+        }
+
+        [Test]
+        public void NextAreSets()
+        {
+            Assert.IsTrue(_tree[_child2Id].Link.Next == _child1Id);
+        }
+
+        [Test]
+        public void ChildrenAreSets()
+        {
+            Assert.IsFalse(_tree[_rootId].Link.Child == _child1Id);
+            Assert.IsTrue(_tree[_rootId].Link.Child == _child2Id);
+        }
+
+        [Test]
+        public void DepthDefined()
+        {
+            Assert.IsTrue(_tree[_rootId].Link.Depth == 0);
+            Assert.IsTrue(_tree[_child1Id].Link.Depth == 1);
+            Assert.IsTrue(_tree[_child2Id].Link.Depth == 1);
+        }
+
+        [Test]
+        public void CtorCapacityOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var tree = new Tree<int>(-42);
+            });
+        }
+    }
+}
